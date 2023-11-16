1961bdeab0bc91a4e683cb06cdf6e843b768f8a6
Jaderdev <jd_pathfinder@hotmail.com>
2023-11-16 18:08:50 -0300

    Primeiro componente funcional

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    title: 'Home',
+    loadComponent: async () => (await import('./pages/home/home.component')).HomeComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+}
