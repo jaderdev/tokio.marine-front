84d8ad03882cb6c571bf36fbc6e0c6216300eee8
Jaderdev <jd_pathfinder@hotmail.com>
2023-11-17 12:48:34 -0300

    Componente últimos agendamentos

--- src/app/lib/components/ultimos-agendamentos/ultimos-agendamentos.component.ts
+++ src/app/lib/components/ultimos-agendamentos/ultimos-agendamentos.component.ts
@@ -2,47 +2,47 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DecimalPipe } from '@angular/common';
 
-interface Country {
-  dataAgendamento: string;
-	name: string;
-	flag: string;
-	area: number;
-	population: number;
-  valor: string;
+interface Transferencias {
+  	dataAgendamento: string;
+	contaOrigem: string;
+	contaDestino: string;
+  	valor: string;
+	taxa: string;
+	dataTransferencia: string;
 }
 
-const COUNTRIES: Country[] = [
+const COUNTRIES: Transferencias[] = [
 	{
     dataAgendamento: "11/10/2023",
-		name: 'Russia',
-		flag: 'f/f3/Flag_of_Russia.svg',
-		area: 17075200,
-		population: 146989754,
-    valor : "R$ 200,00"
+		contaOrigem: '0000001231',
+		contaDestino: '0012921001',
+    	valor : "R$ 200,00",
+		taxa : '10%',
+		dataTransferencia: '16/11/2023'
 	},
 	{
     dataAgendamento: "11/10/2023",
-		name: 'Canada',
-		flag: 'c/cf/Flag_of_Canada.svg',
-		area: 9976140,
-		population: 36624199,
-    valor : "R$ 200,00"
+		contaOrigem: '0000001231',
+		contaDestino: '0012921001',
+    	valor : "R$ 200,00",
+		taxa : '10%',
+		dataTransferencia: '16/11/2023'
 	},
 	{
     dataAgendamento: "11/10/2023",
-		name: 'United States',
-		flag: 'a/a4/Flag_of_the_United_States.svg',
-		area: 9629091,
-		population: 324459463,
-    valor : "R$ 200,00"
+		contaOrigem: '0000001231',
+		contaDestino: '0012921001',
+    	valor : "R$ 200,00",
+		taxa : '10%',
+		dataTransferencia: '16/11/2023'
 	},
 	{
     dataAgendamento: "11/10/2023",
-		name: 'China',
-		flag: 'f/fa/Flag_of_the_People%27s_Republic_of_China.svg',
-		area: 9596960,
-		population: 1409517397,
-    valor : "R$ 200,00"
+		contaOrigem: '0000001231',
+		contaDestino: '0012921001',
+    	valor : "R$ 200,00",
+		taxa : '10%',
+		dataTransferencia: '16/11/2023'
 	},
 ];
 
@@ -54,5 +54,5 @@ const COUNTRIES: Country[] = [
   styleUrls: ['./ultimos-agendamentos.component.css']
 })
 export class UltimosAgendamentosComponent {
-  countries = COUNTRIES;
+  transferencias = [...COUNTRIES, ...COUNTRIES, ...COUNTRIES].slice(1,10);
 }
