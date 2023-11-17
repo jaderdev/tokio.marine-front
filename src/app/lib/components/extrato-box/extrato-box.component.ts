08e9b7fff29edc676f3d6dc31b199274565c1801
Jaderdev <jd_pathfinder@hotmail.com>
2023-11-17 13:01:45 -0300

    Iniciando componente de informação do usuário

--- src/app/lib/components/extrato-box/extrato-box.component.ts
+++ src/app/lib/components/extrato-box/extrato-box.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 import { UltimosAgendamentosComponent } from '../ultimos-agendamentos/ultimos-agendamentos.component';
+import { UserInfoComponent } from '../user-info/user-info.component';
 
 @Component({
   standalone: true,
   selector: 'app-extrato-box',
   templateUrl: './extrato-box.component.html',
   styleUrls: ['./extrato-box.component.css'],
-  imports: [ NgbNavModule, UltimosAgendamentosComponent ]
+  imports: [ NgbNavModule,
+    UltimosAgendamentosComponent,
+    UserInfoComponent ]
 })
 export class ExtratoBoxComponent {
   ngOnInit() {
---
+++ src/app/lib/components/user-info/user-info.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-user-info',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './user-info.component.html',
+  styleUrls: ['./user-info.component.css']
+})
+export class UserInfoComponent {
+
+}
